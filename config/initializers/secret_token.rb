c8c1cf8895d3bf8b02ce547607b27b0462d94c92
Fauzan Ahsan <fauzan@Ozans.local>
2012-10-07 09:54:08 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpiBulog::Application.config.secret_token = 'cebe7d349b74b1a73aa496f2c863a671c6509b2965d206268e47d8ea504f41d61b0e83ebe328667cf0d1099a9d91388163fd0e1d1084cd8109369ca42a6677f3'
