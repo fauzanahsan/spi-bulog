80ab13eecc2be19c7b46c8ac93f758c7831cf85a
Fauzan Ahsan <fauzan@Fauzans.local>
2012-10-23 09:18:31 +0700

    remove index

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,8 @@ SpiBulog::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
   #root to: "admin/dashboard#index"
+  
+  root :to => 'admin/dashboard#index'
 
   # See how all your routes lay out with "rake routes"
 
